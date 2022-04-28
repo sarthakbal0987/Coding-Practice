e3ad5faafe9f5b896041179759f22e7ec1339e9f
Dev-Soumyaranjan <dev.soumya369@gmail.com>
2022-04-28 21:40:05 +0530

    Removing unnecessary variable

--- src/com/online/PrepInsta/String/RemoveVowel.java
+++ src/com/online/PrepInsta/String/RemoveVowel.java
@@ -9,19 +9,20 @@ public class RemoveVowel {
         String str = scan.nextLine().trim();
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
-            if (isVowel(str,i));
-            else
-            {
+            if (isVowel(str, i))
+                ;
+            else {
                 res.append(str.charAt(i));
             }
         }
-        System.out.print("After remove vowel from "+ str+"The result is: "+res);
+        System.out.print("After remove vowel from " + str + "The result is: " + res);
+        scan.close();
     }
-    public static boolean isVowel(String str,int pos){
-          boolean flag = false;
-            return (str.charAt(pos) == 'a' ||str.charAt(pos) == 'e' ||str.charAt(pos) == 'i'||
-                str.charAt(pos) == 'o' ||str.charAt(pos) == 'u' ||str.charAt(pos) =='A' ||
-                str.charAt(pos) =='E'  ||str.charAt(pos) =='I'  ||str.charAt(pos) =='O' ||
-                str.charAt(pos) =='U');
+
+    public static boolean isVowel(String str, int pos) {
+        return (str.charAt(pos) == 'a' || str.charAt(pos) == 'e' || str.charAt(pos) == 'i' ||
+                str.charAt(pos) == 'o' || str.charAt(pos) == 'u' || str.charAt(pos) == 'A' ||
+                str.charAt(pos) == 'E' || str.charAt(pos) == 'I' || str.charAt(pos) == 'O' ||
+                str.charAt(pos) == 'U');
     }
 }

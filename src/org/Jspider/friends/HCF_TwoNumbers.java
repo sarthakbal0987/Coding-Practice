4ad31b5297d942e52a2403affb146dc905f69e2f
Dev-Soumyaranjan <dev.soumya369@gmail.com>
2022-04-28 21:44:09 +0530

    Shifting single line comment to multiline comment for better readibility

--- src/org/Jspider/friends/HCF_TwoNumbers.java
+++ src/org/Jspider/friends/HCF_TwoNumbers.java
@@ -8,26 +8,29 @@ public class HCF_TwoNumbers {
         System.out.println("Enter a number");
         int num = 50;
         int num1 = 100;
-        int j =0;
+        int j = 0;
         int[] arr = new int[num];
         for (int i = 1; i <= num; i++) {
-            if ((num % i ==0) &&(num1 % i == 0)){
+            if ((num % i == 0) && (num1 % i == 0)) {
                 arr[j] = i;
                 j++;
             }
         }
         int hcf = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>hcf){
+            if (arr[i] > hcf) {
                 hcf = arr[i];
             }
         }
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == 0);
-//            else
-//                System.out.print(arr[i]+" ");
-//        }
-        System.out.print("HCF is "+hcf);
+        /*
+         * for (int i = 0; i < arr.length; i++) {
+         * if (arr[i] == 0);
+         * else
+         * System.out.print(arr[i]+" ");
+         * }
+         */
+        System.out.print("HCF is " + hcf);
+        scn.close();
     }
 
 }

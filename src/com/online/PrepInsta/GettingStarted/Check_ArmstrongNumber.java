e7465f903459ae3603db34b57d4f6a6547dd2f1f
Dev-Soumyaranjan <dev.soumya369@gmail.com>
2022-04-28 21:27:11 +0530

    Removing unnecessary variable

--- src/com/online/PrepInsta/GettingStarted/Check_ArmstrongNumber.java
+++ src/com/online/PrepInsta/GettingStarted/Check_ArmstrongNumber.java
@@ -10,29 +10,34 @@ public class Check_ArmstrongNumber {
         int temp = num;
         int count = 0;
         /*
-        * To count the number of digit present in the given number
-        * I use below while loop,In each iteration rem of the number will be
-        * store in rem and count value will be updated.count value will be updated
-        * as many times as we get the remainder
-        */
-        while(num!=0){
-            int rem = num % 10;
-            count++; //
-            num = num / 10; //It will shorten the number by one digit in each iteration until number is zero
+         * To count the number of digit present in the given number
+         * I use below while loop,In each iteration rem of the number will be
+         * store in rem and count value will be updated.count value will be updated
+         * as many times as we get the remainder
+         */
+        while (num != 0) {
+            num = num / 10; // It will shorten the number by one digit in each iteration until number is
+                            // zero
+            count++;
+
         }
-        checkArmstrong(temp,count);//Calling checkArmstrong() method
+        checkArmstrong(temp, count);// Calling checkArmstrong() method
+        scan.close();
 
     }
-    public static void checkArmstrong(int number,int power){
+
+    public static void checkArmstrong(int number, int power) {
         int sum = 0;
         int temp2 = number;
-        while(temp2!=0){
+        while (temp2 != 0) {
             int rem = temp2 % 10;
-            sum = (int)(sum + (Math.pow(rem,power)));
-            temp2 = temp2 /10;
+            sum = (int) (sum + (Math.pow(rem, power)));
+            temp2 = temp2 / 10;
         }
-        if (number == sum) System.out.println("This is an armstrong number");
-        else System.out.println("This is not an armstrong number");
+        if (number == sum)
+            System.out.println("This is an armstrong number");
+        else
+            System.out.println("This is not an armstrong number");
 
     }
 }
